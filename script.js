08c653d5a39b4ba61e90a64984417920dd237721
sultanankrasavchik68 <sultananvarov34@gmail.com>
2025-03-14 21:56:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+alert('Sizda hozir random sonlar chiqishni boshledi')
+alert('Random orqali chiqan son:  49 ')
+alert('biz random sonni 2ga kopaytimoqchi va bo`lmoqchmiz')
+alert('Random orqali chiqan son:49 edi \n biz uni 2ga ko`paytramiz 98 sonni chqardik \n  va shu sonni 2ga bo`lib:49 sonni chqardik \n va shu sonni 3 ga bo`lganda qoldiq: 1 sonni chqardik  ')
